a7d2db1c2bb2840e8816a23bdf1882f7c3442900
jananianandh <jananianandhofficial@gmail.com>
2016-06-07 14:40:06 +0530

    Create reverseint.c

---
+++ reverseint.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int reverse=0,num;
+scanf("%d",&num);
+while (num!=0)
+{
+reverse = reverse * 10;
+reverse = reverse + num%10;
+num= num/10;
+}
+printf("%d",reverse);
+}
