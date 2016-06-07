5880771ffcf8f5885c28b2725f560dd3257cf46d
jananianandh <jananianandhofficial@gmail.com>
2016-06-07 15:53:36 +0530

    Update and rename reverseint.py to intervaleven.py

---
+++ intervaleven.py
@@ -0,0 +1,5 @@
+a=int(input("enter starting value"))
+b=int(input("enter ending value"))
+for x in range(a,b):
+  if (x%2)==0:
+    print(x)
--- reverseint.py
+++
@@ -1,7 +0,0 @@
-reverse=0
-num=int(input("Enter a value"))
-while num!=0:
-reverse = reverse * 10
-reverse = reverse + num%10
-num= num/10
-print(reverse)
